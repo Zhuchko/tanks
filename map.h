fc2be794c06fe91da527f09f4f7b3d419278c70d
zhuchko.egor <zhuchko@gmail.com>
2019-03-21 20:19:18 +0300

    zhuchko

---
+++ map.h
@@ -0,0 +1,10 @@
+#ifndef map_h
+#define map_h
+#include "stdafx.h"
+#include <SFML\Graphics.hpp>
+
+const int HEIGHT_MAP = 25;//размер карты по высоте
+const int WIDTH_MAP = 30;//размер карты по ширине 
+void randomMapGenerate();
+
+#endif
